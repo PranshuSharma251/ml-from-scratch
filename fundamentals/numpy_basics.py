00755f644b92c1d0509a29f9fc5d161aaeb55b82
Pranshu <pranshu241106@gmail.com>
2025-12-23 18:50:55 +0530

    dot product

--- fundamentals/numpy_basics.py
+++ fundamentals/numpy_basics.py
@@ -3,3 +3,17 @@ import numpy as np
 a = np.array([1,2,3,4])
 b = np.array([[1,2,3],[4,5,6]])
 print(b[:,1])
+
+v1 = np.array([1,2,3])
+v2 = np.array([4,5,6])
+dot = np.dot(v1,v2)
+print(dot) #dot product
+print(v1*v2) #elemment vise multiplication 
+
+
+#basic neural network
+W = np.array([[0,1],[1,0],[0,1]]) #weight
+x = np.array([2,3])
+dotp = np.dot(W,x)
+print(dotp)
+print(dotp.shape)
